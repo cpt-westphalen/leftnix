03d82428222d12fa068bd1831b5570873b9d6ad6
rbwestphalen <rbwestphalen@gmail.com>
2022-11-17 10:11:09 -0300

    CSS: Minor MovieCard fixes

--- src/components/Gallery/MovieGalleries/MovieCard/index.tsx
+++ src/components/Gallery/MovieGalleries/MovieCard/index.tsx
@@ -3,9 +3,8 @@ import { BASE_IMG_URL } from "../../../../utils/api";
 
 function useHeroTitle(title: string): [string | null, string] {
 	const heroTitleIndex =
-		title.indexOf(":") + 1 ||
-		(title.toLowerCase().includes("harry potter and") ? 16 : 0);
-
+		(title.toLowerCase().includes("harry potter and") ? 16 : 0) ||
+		title.indexOf(":") + 1;
 	const superTitle = title.slice(0, heroTitleIndex).trim() || null;
 
 	if (!!heroTitleIndex) {
@@ -19,12 +18,15 @@ export const MovieCard = ({ movie }: { movie: MovieTypes }) => {
 	const [superTitle, title] = useHeroTitle(movie.title);
 
 	return (
-		<div className='p-3 bg-dark flex flex-col justify-start items-start flex-shrink-0 basis-52 rounded-sm'>
-			<img src={imgPath}></img>
+		<div className='p-3 max-h-96 bg-dark flex flex-col justify-start items-start flex-shrink-0 flex-grow-0 basis-52 rounded-sm overflow-hidden'>
+			<img
+				className='h-64 w-full object-cover object-top'
+				src={imgPath}
+			/>
 			<h3 className='whitespace-pre'>
 				<span className='text-sm'>{superTitle}</span>
 				{"\n"}
-				<p className='text-2xl'>{title}</p>
+				<p className='text-2xl whitespace-normal'>{title}</p>
 			</h3>
 			{/* <p>{movie.overview}</p> */}
 			<div>--slider--</div>
